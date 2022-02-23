763209b4416bcfe97d59ed86beca48fdfd12612e
RomaK74 <roma07199@gmail.com>
2022-02-24 00:31:16 +0300

    loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+export default class Preloader extends Component{
+    constructor(props){
+        super(props);
+        this.viewRef = React.createRef();
+    }
+    render(){
+        return(
+            <div ref={this.viewRef} className="preloader">
+                <div className="preloader__image_animate"></div>
+            </div>
+        );
+    }
+    componentWillUnmount(){
+        this.viewRef.current.style.opacity = 0;
+    }
+}
